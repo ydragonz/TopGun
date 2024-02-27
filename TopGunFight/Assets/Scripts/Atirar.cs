1afaec547fc4f9e10b3f271f972e376fb1886d29
Thiago Garcia <thiagoncg1@gmail.com>
2024-02-27 13:22:04 -0300

    Balas de metralhadoura
    Criado as balas de metralhadoura...

---
+++ TopGunFight/Assets/Scripts/Atirar.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour
+{
+    //PUBLIC VARIABLES
+    public float velocidade = 100f;
+    public GameObject bala;
+    public Transform cano;
+    public float intervalo = 0.1f;
+    public float forçaBala = 2f;
+
+    //PRIVATE VARIABLES
+    private float tempo;
+
+    private void Update()
+    {
+        //Verifica se a tecla M esta precionada
+        if(Input.GetKey(KeyCode.M))
+        {
+            //incrmente o tempo do tiro
+            tempo += Time.deltaTime;
+
+            //verifica se o tempo decorrido é maior ou menor que o intervalo do termpo do tiro
+            if(tempo >= intervalo)
+            {
+                //instancia a bala pelo cano da metralhadora
+                GameObject novaBala = Instantiate(bala, cano.position, cano.rotation);
+
+                //ajusta a rotaçã da bala, nem sempre é nescessario usar
+                novaBala.transform.Rotate(90,0,0);
+
+                //destroy a balapara ajuste da ememoria
+                Destroy(novaBala, 5);
+
+                //é uma boa pratica dar um get, pegar o componente de fisica da bala antes de usar
+                Rigidbody rb = novaBala.GetComponent<Rigidbody>();
+
+                //aplica uma força de impulso na bala
+                rb.AddForce(cano.forward * velocidade * forçaBala, ForceMode.Impulse);
+
+                //zera o intervalo de termpo 
+                tempo = 0f;
+            }
+        }
+    }
+}
